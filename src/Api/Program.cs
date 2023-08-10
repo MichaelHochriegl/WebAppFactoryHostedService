6b08e399b165452d9f8e00bbdabb6fc4ace29204
MichaelHochriegl <michael@hochriegl.dev>
2023-08-10 22:51:23 +0200

    add post endpoint for movies

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -15,5 +15,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/movies", (MovieDbContext dbContext) => dbContext.Movies.ToListAsync());
+app.MapPost("/api/movies", async (Movie movie, MovieDbContext dbContext) =>
+{
+    dbContext.Movies.Add(movie);
+    await dbContext.SaveChangesAsync();
+});
 
 app.Run();
