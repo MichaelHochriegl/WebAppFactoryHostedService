c643c63026f62f8fd42eedef29e3513030c2c5d4
Pascal Isenschmid <pascal@isenschmid.com>
2023-08-12 21:07:52 +0200

    feat: fix integrationtest setup
    s

--- tests/Api.Tests.Integration/UnitTest1.cs
+++ tests/Api.Tests.Integration/UnitTest1.cs
@@ -2,10 +2,11 @@ using System.Net;
 using System.Net.Http.Json;
 using Api.Data;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.Tests.Integration;
 
-public class UnitTest1 : IClassFixture<MovieWebApplicationFactory>
+public class UnitTest1 : IClassFixture<MovieWebApplicationFactory>, IAsyncLifetime
 {
     private readonly MovieWebApplicationFactory _factory;
     private readonly HttpClient _client;
@@ -29,5 +30,20 @@ public class UnitTest1 : IClassFixture<MovieWebApplicationFactory>
         var content = await result.Content.ReadFromJsonAsync<IEnumerable<Movie>>();
         content.Should().NotBeNull();
         content!.Count().Should().Be(2);
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<MovieDbContext>();
+        dbContext.Movies.Add(new Movie() { Name = "Ace Ventura" });
+        dbContext.Movies.Add(new Movie() { Name = "Hot Shots" });
+
+        await dbContext.SaveChangesAsync().ConfigureAwait(false);
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
     }
 }
